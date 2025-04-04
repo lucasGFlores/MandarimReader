69b8fa6651b0d96b4fec347e506ceb09259ca84b
lucasGFlores <lucasgabrielflores25@gmail>
2025-04-03 22:10:32 -0300

    update: The logic to save the tree of Ceddict dictionary is give to Archiver

--- src/searchers/cccedict/dictionary.py
+++ src/searchers/cccedict/dictionary.py
@@ -7,16 +7,9 @@ from threading import Lock
 from src import Hanzi
 from ..binary_tree_hanzi import BinaryTreeHanzi
 from .parser import get_list
-
-class OldTreeFileError(Exception):
-    def __init__(self, motive):
-        self.add_note(f"Some files are in old versions\nexcuse: {motive}")
+from ...utils.archiver.archiver import Archiver, DontHavePathError
 
 
-def list_files(path):
-    pasta = Path(path)
-    return sorted([arq.resolve() for arq in pasta.glob('**/*') if arq.is_file()],
-                  key=lambda x: int(re.findall(r'\d+', str(x))[0]))
 
 
 class SingletonMeta(type):
@@ -38,7 +31,11 @@ class CeDictionary(metaclass=SingletonMeta):
     def __init__(self):
         super().__init__()
         self._tree_pool: dict[int:BinaryTreeHanzi] = {}
-        self._feed_tree_pool()
+        try:
+           self._tree_pool = Archiver().load(CeDictionary)
+        except DontHavePathError:
+            self._generate_list()
+            Archiver().save(CeDictionary,self._tree_pool)
 
     def search_data(self, hanzi_text: str) -> list:
         text_size = len(hanzi_text)
@@ -58,27 +55,6 @@ class CeDictionary(metaclass=SingletonMeta):
     def _get_tree_from_pool(self, index: int) -> BinaryTreeHanzi:
         return self._tree_pool.setdefault(index, BinaryTreeHanzi())
 
-    def _feed_tree_pool(self):
-
-        if os.path.exists(self._dir_pool) and list_files(self._dir_pool) != []:
-            try:
-                for pos, file in enumerate(list_files(self._dir_pool)):
-                    self._tree_pool[pos] = self._load_dictionary(file)
-            except OldTreeFileError:
-                self._generate_list()
-        else:
-            self._generate_list()
-
-    @staticmethod
-    def _load_dictionary(path) -> BinaryTreeHanzi | None:
-        try:
-            with open(path, 'rb') as file:
-                return pickle.loads(file.read())
-        except ModuleNotFoundError as e:
-            logging.warning(e)
-            print("trying to load old written files, need to convert again")
-            raise OldTreeFileError(e)
-
     def _generate_list(self):
         for info in get_list():
             hanzi_length = len(info['simplified'])
@@ -88,15 +64,3 @@ class CeDictionary(metaclass=SingletonMeta):
                 pinyin=info['pinyin'],
                 description=[info['english']]
             ))
-
-    def save_dictionary(self):
-        if not os.path.exists(self._dir_pool):
-            os.makedirs(self._dir_pool)
-        for pos, tree_ in sorted(self._tree_pool.items(), key=lambda x: x[0]):
-            self._write_dictionary(tree_, self._dir_pool.joinpath(f"shelf{pos}.pickle"))
-
-    @staticmethod
-    def _write_dictionary(tree, path):
-        os.makedirs(path.parent, exist_ok=True)
-        with open(path, 'wb') as file:
-            file.write(pickle.dumps(tree))  #it is to work, but the type system is yelling about that
